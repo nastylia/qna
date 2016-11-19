9e3cda93173f93d94d1cc1f8e9d1505dfc990be0
Anastasiya <nastylia@gmail.com>
2016-11-19 17:32:46 +0300

    added forgoten blank

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
   it { should validate_presence_of :body }
-  it { should validate_presence_of :question}
+  it { should validate_presence_of :question }
 end
