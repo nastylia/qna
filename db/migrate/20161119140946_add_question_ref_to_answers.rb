400f60cd09c55ae7494d9b05e9157b3584d7c8c3
Anastasiya <nastylia@gmail.com>
2016-11-19 17:15:10 +0300

    added tests for associations and associations

---
+++ db/migrate/20161119140946_add_question_ref_to_answers.rb
@@ -0,0 +1,5 @@
+class AddQuestionRefToAnswers < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :answers, :question, foreign_key: true, index: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119133351) do
+ActiveRecord::Schema.define(version: 20161119140946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "question_id"
+    t.index ["question_id"], name: "index_answers_on_question_id", using: :btree
   end
 
   create_table "questions", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 20161119133351) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
