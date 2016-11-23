dbba3140db22c65333dafb82a2ff86961d5e080f
Anastasiya <nastylia@gmail.com>
2016-11-24 01:13:04 +0300

    added tests and actions for show new create

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -11,9 +11,59 @@ RSpec.describe QuestionsController, type: :controller do
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
     end
-  
+
     it 'renders index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+
+    before { get :show, id: question }
+
+    it 'assigns the requested question to @question' do
+      get :show, id: question
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show views' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves new question in the db' do
+        expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
+      end
+      it 'redirects to show view' do
+        post :create, question: attributes_for(:question)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect { post :create, question: attributes_for(:invalid_question) }.to_not change(Question, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, question: attributes_for(:invalid_question)
+        expect(response).to render_template :new
+      end
     end
   end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,4 +1,9 @@
 FactoryGirl.define do
+  factory :invalid_question, class: Question do
+    title nil
+    body nil
+  end
+
   factory :question do
     title "MyString"
     body "MyText"
