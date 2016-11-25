de3f10d5baead6272646ed49b7832ff151bdd71e
Anastasiya <nastylia@gmail.com>
2016-11-25 23:06:40 +0300

    deleted validation and test for assocoation answer-question

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
   it { should validate_presence_of :body }
-  it { should validate_presence_of :question }
   it { should have_db_index :question_id }
 end
