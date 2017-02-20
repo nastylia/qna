d4f440c6694df907d8375d110bc34a0765b4e495
Anastasiya <nastylia@gmail.com>
2017-02-20 23:06:50 +0300

    added feature test for 'Users can view questions answers'

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,19 @@
 FactoryGirl.define do
+
+  # sequence :body  do |n|
+  #   "Awesome answer #{n}"
+  # end
+
   factory :answer do
-    body "MyText"
+    body
+    question
+
+    # factory :answer_with_question do
+    #   after(:create) do |answer|
+    #     answer.question << :question
+    #   end
+    # end
+
   end
 
   factory :invalid_answer, class: Answer do
