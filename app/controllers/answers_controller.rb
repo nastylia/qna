ebde73e8c9e7d14b931a515046bcafb6e174ab43
Anastasiya <nastylia@gmail.com>
2017-02-24 20:11:28 +0300

    started to edit comments

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,29 +1,26 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :load_answer, only: [:destroy]
-  def index
-    @answers = Answer.where(question_id: params[:question_id])
-  end
-
-  def new
-    @answer = Answer.new
-  end
 
   def create
     @question = Question.find(params[:question_id])
     @answer = @question.answers.new(answer_params)
     @answer.author_id = current_user.id
-    if @answer.save
-      redirect_to @question
-    else
-      render :new
+    unless @answer.save
+      flash[:notice] = 'Answer was not saved'
     end
+    redirect_to @question
   end
 
   def destroy
-    @question = Question.find(@answer.question_id)
-    @answer.destroy
-    flash[:notice] = 'Answer was successfully deleted'
+    # binding.pry
+    @question = @answer.question
+    if user_signed_in? && current_user.id == @answer.author_id
+      @answer.destroy
+      flash[:notice] = 'Answer was successfully deleted'
+    else
+      flash[:notice] = 'You are not authorized to delete question'
+    end
     redirect_to @question
   end
 
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,7 +1,7 @@
 module ControllerMacros
-  def sign_in_user
+  def sign_in_user(user)
     before do
-      @user = create(:user)
+      @user = user
       @request.env['devise.mapping'] = Devise.mappings[:user]
       sign_in @user
     end
