88087d0d1e6e0c9fc64ed27fdb4a736963fb5cae
Anastasiya <nastylia@gmail.com>
2017-03-29 23:46:34 +0300

    added ability to add several files to new question and answer

--- spec/feature/answers/add_files_to_answer_spec.rb
+++ spec/feature/answers/add_files_to_answer_spec.rb
@@ -22,8 +22,25 @@ feature 'Add files to answer', %q{
     within '.answers' do
       expect(page).to have_link 'test1', href: "/uploads/attachment/file/1/test1"
     end
-
   end
+
+  scenario 'User adds several files when he answers', js: true do
+    fill_in 'Your answer', with: 'test body answer'
+
+    click_on 'Add file'
+
+    inputs = all('input[type="file"]')
+    inputs[0].set("#{Rails.root}/spec/test_files/test1")
+    inputs[1].set("#{Rails.root}/spec/test_files/test2")
+    click_on 'Post Your Answer'
+
+    within '.answers' do
+      expect(page).to have_link 'test2', href: "/uploads/attachment/file/2/test2"
+      expect(page).to have_link 'test1', href: "/uploads/attachment/file/1/test1"
+    end
+    
+  end
+
 
   
 end
--- spec/feature/questions/add_files_to_question.rb
+++ spec/feature/questions/add_files_to_question.rb
@@ -23,5 +23,21 @@ feature 'Add files to question', %q{
 
   end
 
+  scenario 'User adds several files when he asks question', js: true do
+    fill_in 'Title', with: 'My awesome question'
+    fill_in 'Body', with: 'test'
+
+    click_on 'Add file'
+
+    inputs = all('input[type="file"]')
+    inputs[0].set("#{Rails.root}/spec/test_files/test1")
+    inputs[1].set("#{Rails.root}/spec/test_files/test2")
+    click_on 'Create'
+
+    expect(page).to have_link 'test2', href: "/uploads/attachment/file/3/test2"
+    expect(page).to have_link 'test1', href: "/uploads/attachment/file/2/test1"
+
+  end
+
   
 end
