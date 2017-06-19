2f8ceabe26e82d34b641b7f23921853403c48679
Anastasiya <nastylia@gmail.com>
2017-06-19 20:32:50 +0300

    fixed comments

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -3,31 +3,30 @@ module Votable
 
   included do
     has_many :votes, as: :votable, dependent: :delete_all
+  end
 
-    def sum_votes
-      self.votes.sum(:value)
-    end
+  def sum_votes
+    self.votes.sum(:value)
+  end
 
-    def vote(value:, current_user: )
-      set_vote(current_user)
-      if @vote.nil?
-        @vote = Vote.create(value: value, user: current_user, votable_type: self.class.name, votable_id: self.id)
-        return @vote
-      end
-    end
-
-    def unvote(current_user:)
-      set_vote(current_user)
-      if @vote
-        @vote.delete
-        return @vote
-      end
-    end
-
-    private
-
-    def set_vote(current_user)
-      @vote = Vote.where(user: current_user, votable_type: self.class.name, votable_id: self.id).first
+  def vote(value:, current_user: )
+    set_vote(current_user)
+    if @vote.nil?
+      self.votes.create(user: current_user, value: value)
     end
   end
+
+  def unvote(current_user:)
+    set_vote(current_user)
+    if @vote
+      @vote.destroy
+    end
+  end
+
+  private
+
+  def set_vote(current_user)
+    @vote = self.votes.where(user: current_user).first
+  end
+
 end
