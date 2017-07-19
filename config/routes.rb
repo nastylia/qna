0fb09f3e881fc08a1b6b655e397c7477358c70d7
Anastasiya <nastylia@gmail.com>
2017-07-19 14:26:19 +0300

    added profile/me

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks'}
 
   concern :votable do
@@ -20,6 +21,14 @@ Rails.application.routes.draw do
   resources :attachments, only: :destroy
   devise_scope :user do
     post :confirm_email, action: :confirm_email, controller: 'omniauth_callbacks'
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resource :profiles do
+        get :me, on: :collection
+      end
+    end
   end
 
   root to: "questions#index"
